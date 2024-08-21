d9bac45d75ea56eb3124975ab61ab5e36cdd6546
Jens <jenstheminer@gmail.com>
2024-08-21 16:51:31 +0200

    V1.5 Beta 5
    Ticket system is now better WIP

--- src/main/java/dev/kippie/commands/Ticket.java
+++ src/main/java/dev/kippie/commands/Ticket.java
@@ -3,6 +3,7 @@ package dev.kippie.commands;
 import io.github.cdimascio.dotenv.Dotenv;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.emoji.Emoji;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
@@ -16,44 +17,52 @@ public class Ticket extends ListenerAdapter {
     @Override
     public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
         if (event.getName().equals("ticket")) {
-            Dotenv dotenv = Dotenv.load();
-            String ticket_category = dotenv.get("TICKET_CATEGORY");
-            String ticketname = "ticket-" + event.getUser().getName();
-            List tickets = event.getGuild().getTextChannelsByName(ticketname, true);
-            if (!tickets.isEmpty()) {
-                event.reply("You already have a ticket open!").setEphemeral(true).queue();
-
-            } else {
-                event.getGuild().getCategoryById(ticket_category).createTextChannel(ticketname).queue(channel -> {
-                    channel.upsertPermissionOverride(event.getMember()).grant(Permission.VIEW_CHANNEL).queue();
-                    event.reply("Ticket has been created! <#" + channel.getId() + ">").setEphemeral(true).queue();
+            if (event.getGuild().getTextChannelsByName("ticket-" + event.getUser().getName(), true).isEmpty()) {
+                event.getUser().openPrivateChannel().map(channel -> {
                     EmbedBuilder embed = new EmbedBuilder();
-                    embed.setTitle("Ticket " + event.getUser().getName());
-                    String color = dotenv.get("COLOR");
-                    embed.setColor(Color.decode(color));
-                    String name = dotenv.get("BOT_NAME");
-                    embed.setFooter(name);
-                    embed.addField("Closing this ticket:", "Click on the red button! Be warned, when the ticket is deleted it's gone forever!", false);
-                    embed.addField("Adding another user to this ticket:", "If you wish to add someone to this ticket use /add.", false);
+                    embed.setTitle("Open a ticket.");
+                    embed.setDescription("Please select one of the ticket subjects below.");
                     channel.sendMessageEmbeds(embed.build())
                             .addActionRow(
-                                    net.dv8tion.jda.api.interactions.components.buttons.Button.danger("close", "Close").withEmoji(Emoji.fromUnicode("U+274C"))
-                            )
+                                    net.dv8tion.jda.api.interactions.components.buttons.Button.danger("report", "Report").withEmoji(Emoji.fromUnicode("U+26A0")),
+                                    net.dv8tion.jda.api.interactions.components.buttons.Button.primary("questions", "Questions").withEmoji(Emoji.fromUnicode("U+2753")),
+                                    net.dv8tion.jda.api.interactions.components.buttons.Button.secondary("other", "Other").withEmoji(Emoji.fromUnicode("U+1F4AC"))
+                            ).queue();
+                    event.reply("Please check your personal dm's to create a ticket.").setEphemeral(true).queue();
+                    return null;
+                }).queue();
 
-                            .queue();
-                });
-
-
-
-
+            } else {
+                event.reply("You already have a ticket open! <#" + event.getGuild().getTextChannelsByName("ticket-" + event.getUser().getName(), true).getFirst().getId() + ">" ).setEphemeral(true).queue();
             }
+
+
+
+
+
+
         }
     }
 
     @Override
     public void onButtonInteraction(ButtonInteractionEvent event) {
+        Dotenv dotenv = Dotenv.load();
         if (event.getComponentId().equals("close")) {
-            event.getChannel().delete().queue();
+
+        } else if (event.getComponentId().equals("report")) {
+            Guild guild = event.getJDA().getGuildById(dotenv.get("GUILD_ID"));
+            if (guild.getTextChannelsByName("ticket-" + event.getUser().getName(), true).isEmpty()) {
+
+
+                guild.createTextChannel("ticket-" + event.getUser().getName()).queue(textChannel -> {
+                    event.reply("Your ticket has been made! <#" + textChannel.getId() + ">").setEphemeral(true).queue();
+                });
+
+
+            } else {
+                event.reply("You already have a ticket open! <#" + event.getGuild().getTextChannelsByName("ticket-" + event.getUser().getName(), true).getFirst().getId() + ">" ).setEphemeral(true).queue();
+            }
+
         }
     }
 }
