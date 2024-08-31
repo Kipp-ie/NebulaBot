99ae21fe40579a2eda1dcde50b44b36d8f7d3b7c
Jens <jenstheminer@gmail.com>
2024-08-31 22:51:35 +0200

    V1.5 Beta 16
    Fixed stuff

--- src/main/java/dev/kippie/commands/Give.java
+++ src/main/java/dev/kippie/commands/Give.java
@@ -32,6 +32,34 @@ public class Give extends ListenerAdapter {
                         Updates.set("money", "100")
                 );
                 collection.updateOne(Objects.requireNonNull(collection.find(eq("id", user.getId())).first()), updates);
+            } else {
+                Document doc2 = collection.find(eq("id", event.getUser().getId())).first();
+                if (doc2 == null) {
+                    collection.insertOne(new Document("id", user.getId()));
+                    Bson updates = Updates.combine(
+                            Updates.set("money", "100")
+                    );
+                    collection.updateOne(Objects.requireNonNull(collection.find(eq("id", event.getUser().getId())).first()), updates);
+
+                }
+                Document doc3 = collection.find(eq("id", event.getUser().getId())).first();
+                if (Integer.parseInt(doc3.get("money").toString()) >= event.getOption("amount").getAsInt()) {
+                    collection.insertOne(new Document("id", user.getId()));
+                    Bson updates = Updates.combine(
+                            Updates.set("money", Integer.parseInt(doc3.get("money").toString()) - event.getOption("amount").getAsInt())
+                    );
+                    collection.updateOne(Objects.requireNonNull(collection.find(eq("id", event.getUser().getId())).first()), updates);
+
+                    Document doc4 = collection.find(eq("id", user.getId())).first();
+                    Bson updates2 = Updates.combine(
+                            Updates.set("money", Integer.parseInt(doc4.get("money").toString()) + event.getOption("amount").getAsInt())
+                    );
+                    collection.updateOne(Objects.requireNonNull(collection.find(eq("id", event.getUser().getId())).first()), updates2);
+                    event.reply("You gave " + event.getOption("amount").getAsInt() + " to " + event.getOption("user").getAsUser().getName()).queue();
+                } else {
+                    event.reply("You do not have enough money!").setEphemeral(true).queue();
+                }
+
+            }
         }
-    }
-}
+    }}
